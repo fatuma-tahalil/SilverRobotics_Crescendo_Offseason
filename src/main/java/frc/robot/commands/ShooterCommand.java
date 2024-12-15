aa324b567b4714a10f8362870c44e51aefcc25a5
FatumaT <regretmakingthis@gmail.com>
2024-12-14 21:02:40 -0500

    Removed wrong shooter import

--- src/main/java/frc/robot/commands/ShooterCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.ShooterConstants;
 import frc.robot.subsystems.ShooterSubsystem;
-import frc.robot.subsystems.shooter;
 
 public class ShooterCommand extends Command {
   /**Creates a new ShooterCommand */
